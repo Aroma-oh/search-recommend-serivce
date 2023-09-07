74ff7a2bf26d9bf3c2236599dd5219e5a9795318
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-09-07 20:09:30 +0900

    feat: 리스트 선택 커스텀 훅 생성

---
+++ src/hooks/useSelectList.ts
@@ -0,0 +1,28 @@
+import {useRef, useState} from 'react';
+
+export const useListSelect = (isEmptyInput: boolean) => {
+    const [selectListIdx, setSelectListIdx] = useState(-1);
+    const selectRef = useRef<HTMLUListElement>(null);
+
+    const updateSelectIdx = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.nativeEvent.isComposing) return;
+
+        if (isEmptyInput) {
+            setSelectListIdx(-1);
+        }
+
+        switch (event.key) {
+            case 'ArrowUp':
+                if (selectListIdx < 0) return;
+                setSelectListIdx(prev => prev - 1);
+                if (selectListIdx <= 0) setSelectListIdx(-1);
+                break;
+            case 'ArrowDown':
+                setSelectListIdx(prev => prev + 1);
+                if (selectRef.current?.childElementCount === selectListIdx + 1) setSelectListIdx(0);
+                break;
+        }
+    };
+
+    return {selectListIdx, updateSelectIdx, selectRef};
+};
