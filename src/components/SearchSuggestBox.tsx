e80e8bcd30fd1adcd393c9650eb00a4d5206398a
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-09-07 20:07:16 +0900

    feat: 추천 검색어 컴포넌트 생성

---
+++ src/components/SearchSuggestBox.tsx
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+import {Api} from 'types/api';
+import {AiOutlineSearch} from 'react-icons/ai';
+import {RefObject} from 'react';
+
+interface SearchSuggestBoxProps {
+    dataState: Api;
+    isEmptyInput: boolean;
+    selectRef: RefObject<HTMLUListElement>;
+    selectListIdx: number;
+    value: string;
+}
+
+const SearchSuggestBox = ({
+    dataState,
+    isEmptyInput,
+    selectRef,
+    selectListIdx,
+    value,
+}: SearchSuggestBoxProps) => {
+    if (dataState.status === 'ERROR' || !dataState.data)
+        return <Box>결과를 불러오는데 실패했습니다. </Box>;
+
+    return (
+        <Box>
+            <ul ref={selectRef}>
+                {isEmptyInput ? (
+                    <p>최근 검색어가 없습니다.</p>
+                ) : (
+                    <>
+                        <li>
+                            <AiOutlineSearch /> {value}
+                        </li>
+                        <p>추천 검색어</p>
+                        {dataState.data.map((item, index) => (
+                            <li key={index} className={selectListIdx === index ? 'selected' : ''}>
+                                <AiOutlineSearch /> {item}
+                            </li>
+                        ))}
+                    </>
+                )}
+            </ul>
+        </Box>
+    );
+};
+
+export default SearchSuggestBox;
+
+const Box = styled.div`
+    width: 450px;
+    min-height: 140px;
+
+    margin-top: 6px;
+    padding: 20px 0;
+    box-sizing: border-box;
+
+    background: var(--white);
+    box-shadow: var(--box-shadow);
+    border-radius: 14px;
+
+    p {
+        color: var(--gray);
+        padding: 8px 20px;
+        font-size: 12px;
+    }
+    li {
+        padding: 12px 20px;
+    }
+
+    .selected {
+        background-color: var(--bg-gray);
+    }
+`;
