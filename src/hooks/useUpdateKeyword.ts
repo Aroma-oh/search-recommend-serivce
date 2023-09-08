268af37d5da1511f1bf187fe40d8f5849cf72d33
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-09-08 11:17:52 +0900

    feat: 한/영 분리하여 엄격하게 키워드 업데이트

--- src/hooks/useUpdateKeyword.ts
+++ src/hooks/useUpdateKeyword.ts
@@ -1,28 +1,33 @@
 import {useRef} from 'react';
 import {useSetRecoilState} from 'recoil';
 import {searchKeywordState} from 'store/atom';
-import {getLastWordAfterSpace, debounce, isKorean, keepKoreanSyllables} from 'utils/searchHelpers';
+import {
+    getLastWordAfterSpace,
+    debounce,
+    isKorean,
+    keepKoreanSyllables,
+    isEnglish,
+} from 'utils/searchHelpers';
 
-const DEBOUNCE_TIMING = 100;
+const DEBOUNCE_TIMING = 400;
 
 export const useUpdateKeyword = () => {
     const setSearchKeyword = useSetRecoilState(searchKeywordState);
 
     const debouncedSearch = useRef(
-        debounce((value: string) => {
-            const handledValue = getLastWordAfterSpace(value);
-
-            if (isKorean(handledValue) && handledValue.length > 0) {
-                const koValue = keepKoreanSyllables(handledValue);
-                setSearchKeyword(koValue);
-            } else {
-                setSearchKeyword(handledValue);
-            }
+        debounce((handledValue: string) => {
+            setSearchKeyword(handledValue);
         }, DEBOUNCE_TIMING)
     ).current;
 
     const updateKeyword = (value: string) => {
-        debouncedSearch(value);
+        const handledValue = getLastWordAfterSpace(value);
+
+        if (isEnglish(handledValue)) debouncedSearch(handledValue);
+        if (isKorean(handledValue) && handledValue.length > 0) {
+            const koValue = keepKoreanSyllables(handledValue);
+            setSearchKeyword(koValue);
+        }
     };
 
     return updateKeyword;
