06c06fd1d72bdf474174c781ae2f14a3528d624b
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-09-08 13:28:51 +0900

    refator: 불필요한 코드 제거

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -44,10 +44,3 @@ const Input = styled.input`
 
     font-size: 16px;
 `;
-
-// searchKeyword 핸들링 : updateKeyword
-// (공통) 띄어쓰기 : 마지막 단어 기준으로 추천 검색어 제공
-// (영어) 디바운싱
-// (한글) 마지막 음절이 완전할때 완전하지 않은 음절 제거
-
-// + 버튼 클릭할때도  updateKeyword되도록 추가 필요
