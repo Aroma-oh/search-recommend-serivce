297890fc167a70a0f1aec1f462d190a1e66f451b
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-09-08 21:24:23 +0900

    style: 주석제거

--- src/hooks/useCacheStore.ts
+++ src/hooks/useCacheStore.ts
@@ -1,16 +1,19 @@
 import {useRecoilState, useSetRecoilState} from 'recoil';
 import {cacheStoreState, dataState} from 'store/atom';
 import {useFetch} from './useFetch';
-import {useCallback} from 'react';
+import {useCallback, useEffect} from 'react';
 
 const ONE_DAY_MS = 24 * 60 * 60 * 1000;
-// const ONE_DAY_MS = 5000;
 
 export const useCacheStore = () => {
     const [cacheStore, setCacheStoreState] = useRecoilState(cacheStoreState);
     const setData = useSetRecoilState(dataState);
 
     const fetchData = useFetch();
+
+    useEffect(() => {
+        console.info('cacheStore', cacheStore); // cacheStore 확인을 위한 콘솔입니다.
+    }, [cacheStore]);
 
     const caching = useCallback(
         async (searchKeyword: string) => {
@@ -36,5 +39,3 @@ export const useCacheStore = () => {
 
     return caching;
 };
-
-// 키 - 값 제대로 캐싱 안됨
